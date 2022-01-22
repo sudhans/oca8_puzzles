0e074d35e80635a27aa2e0a455598258ab58938d
msd <sudhans@gmail.com>
2022-01-22 21:02:09 +0530

    Lamda Exercise for HackerRank

---
+++ src/com/msd/hackerrank/lamda01.java
@@ -0,0 +1,81 @@
+package com.msd.hackerrank;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class lamda01 {
+    public static void main(String[] args) throws IOException {
+
+        MyMath ob = new MyMath();
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int T = Integer.parseInt(br.readLine());
+        PerformOperation op;
+        boolean ret = false;
+        String ans = null;
+        while (T--> 0) {
+            String s = br.readLine().trim();
+            StringTokenizer st = new StringTokenizer(s);
+            int ch = Integer.parseInt(st.nextToken());
+            int num = Integer.parseInt(st.nextToken());
+            if (ch == 1) {
+                op = ob.isOdd();
+                ret = ob.checker(op, num);
+                ans = (ret) ? "ODD" : "EVEN";
+            } else if (ch == 2) {
+                op = ob.isPrime();
+                ret = ob.checker(op, num);
+                ans = (ret) ? "PRIME" : "COMPOSITE";
+            } else if (ch == 3) {
+                op = ob.isPalindrome();
+                ret = ob.checker(op, num);
+                ans = (ret) ? "PALINDROME" : "NOT PALINDROME";
+
+            }
+            System.out.println(ans);
+        }
+    }
+
+}
+
+interface PerformOperation {
+    boolean check(int a);
+}
+class MyMath {
+    public static boolean checker(PerformOperation p, int num) {
+        return p.check(num);
+    }
+
+    public PerformOperation isOdd() {
+        return  a -> a % 2 != 0;
+    }
+
+    public PerformOperation isPrime() {
+        return a -> isPrime(a);
+    }
+
+    public PerformOperation isPalindrome() {
+        return a -> isPalindrome(a);
+    }
+
+    private boolean isPalindrome(int number) {
+        String numberStr = String.valueOf(number);
+        return numberStr.equals(new StringBuffer(numberStr).reverse().toString());
+    }
+
+    private boolean isPrime(int number) {
+        if (number < 2) {
+            return false;
+        }
+
+        for (int i=2;i<number;i++) {
+            if (number % i == 0) {
+                return false;
+            }
+        }
+
+        return true;
+    }
+    // Write your code here
+}
